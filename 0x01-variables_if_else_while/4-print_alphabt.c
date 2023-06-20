26e60f9725cd89fe777cd35a123e72d7062bb485
farah hany <fahany300@gmail.com>
2023-06-19 17:30:42 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all alphabet letters except q and e
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
