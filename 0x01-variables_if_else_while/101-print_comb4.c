84413bf40fc0e0fe683d7d332636dae4af0092bb
farah hany <fahany300@gmail.com>
2023-06-19 18:37:17 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/***/
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of three digits.
+ *
+ * Return: always 0
+*/
 
 int main(void)
 {
