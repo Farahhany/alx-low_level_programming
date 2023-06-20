901386ec5047a50afed3b12421f251094d5a6773
farah hany <fahany300@gmail.com>
2023-06-20 12:44:49 -0700

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a spacific statement
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	printf("\_putchar\n");
+	return (0);
+}
