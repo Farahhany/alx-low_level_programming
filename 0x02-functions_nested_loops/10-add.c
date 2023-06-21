124afd48dc9403ff155b2142643a8244b0c2e43c
farah hany <fahany300@gmail.com>
2023-06-20 19:59:30 -0700

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add 2 numbers
+ *
+ * @a: first number
+ *
+ * @b second number
+ *
+ * Return: sum
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
