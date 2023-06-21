56b4e936f6ed27471c4b250cc93df3c810d8397b
farah hany <fahany300@gmail.com>
2023-06-20 19:49:10 -0700

    c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 time tables
+*/
+
+void times_table(void)
+{
+	int n, m, d;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(48);
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(',');
+
+			p = n * m;
+
+			if (p <= 9)
+				_putchar(' ');
+			else
+				_putchar((p / 10) + 48);
+			_putchar((p % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
