0481eec652dc2f1e155fb88ed36cd089653faf34
farah hany <fahany300@gmail.com>
2023-06-22 19:39:21 -0700

    c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ *
+ * @a: firts integer
+ * @b: second integer
+ *
+ * Return: result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: always 0
+*/
+
+void more_numbers(void)
+{
+
