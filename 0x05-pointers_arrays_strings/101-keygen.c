f8c04b37d85f8cefa9bf13e33abc59cf366ccab1
farah hany <fahany300@gmail.com>
2023-06-27 02:52:56 -0700

    c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "time.h"
+
+/**
+ * main - generates random valid passwords
+ *
+ * Return: zero
+*/
+
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 120;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}
