7c73e2fcb3b959276d5aaa72336cb2a969beff4e
farah hany <fahany300@gmail.com>
2023-07-05 14:40:10 -0700

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,21 +3,22 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: input
+ * @v: input 2
  * Reurn: int
+ * Return: int
 */
+
 int square(int n, int v);
 int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
 }
-
 /**
  * square - find square root
  * @n: input 1
  * @v: input 2
  * Return: int
 */
-
 int square(int n, int v)
 {
 	if (v * v == n)
