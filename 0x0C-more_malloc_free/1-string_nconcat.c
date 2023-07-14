5590a9d0ebe2af6383f11a6083042cd5b49448f7
farah hany <fahany300@gmail.com>
2023-07-14 16:06:57 -0700

    c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *string_nconcat - concatenates two strings.
+ * @s1: char
+ * @s2: char
+ * @n: int
+ * Return: pointer
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
