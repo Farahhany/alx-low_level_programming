013b3a40c7855d7b15086acac77599d63ebdcf6d
farah hany <fahany300@gmail.com>
2023-07-18 14:52:18 -0700

    c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	chat *owner;
+}
